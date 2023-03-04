fe2785e7e14229295dfa92f4e69c019d8f9cf1ad
Neal Fultz <nfultz@gmail.com>
2023-03-04 21:16:25 +0000

    implementing deck and weather modules

---
+++ components/deck.py
@@ -0,0 +1,77 @@
+import random
+
+
+SUITS = "CDHS"
+RANKS = "A234567890JQK"
+
+class Card(str):
+
+    @property
+    def rank(self):
+        return self[0]
+
+    @property
+    def suit(self):
+        return self[1]
+
+    @property
+    def value(self):
+        if self.rank == 'x':
+            return 14
+        return 1 + RANKS.find(self.rank)
+
+class Deck(list):
+
+    def __init__(self):
+        self += [Card(r+s) for s in SUITS for r in RANKS]
+        self.append(Card("xS"))
+        self.append(Card("xH"))
+        self._top = len(self)
+        self._bottom = 0
+
+    def shuffle(self):
+        random.shuffle(self)
+
+    @property
+    def drift(self):
+        return (self._top - self._bottom) // 2
+
+    @property
+    def heat(self):
+        temp = lambda x: x.value - 7
+        x =  sum(map(temp, self[:self._bottom]), 100)
+        y =  sum(map(temp, self[self._top - 1:]), 100)
+        if x == y:
+            x = x-2
+        return sorted((x,y))
+
+    @property
+    def top(self):
+        j = self._top - 1
+        i = random.randrange(0, self._top)
+        self[i], self[j] = self[j], self[i]
+
+        self._top = self._top - 1
+        if self._top == 0:
+            self._top = len(self)
+
+        return self[j]
+
+    @property
+    def bottom(self):
+        j = self._bottom
+        i = random.randrange(j, len(self))
+        self[i], self[j] = self[j], self[i]
+
+        self._bottom = self._bottom + 1
+        if self._bottom == len(self):
+            self._bottom = 0
+
+        return self[j]
+
+
+
+
+
+
+
---
+++ components/weather.py
@@ -0,0 +1,40 @@
+
+
+def sand_storm():
+    pass
+
+def wind_at_your_back():
+    pass
+
+def calm_breeze():
+    pass
+
+def extreme_sun():
+    pass
+
+def spice_clouds():
+    pass
+
+def noxious_miasma():
+    pass
+
+def rain():
+    pass
+
+
+WEATHER_EVENTS = {
+  'A': sand_storm,
+  '2': wind_at_your_back,
+  '3': wind_at_your_back,
+  '4': wind_at_your_back,
+  '5': wind_at_your_back,
+  '6': calm_breeze,
+  '7': calm_breeze,
+  '8': calm_breeze,
+  '9': calm_breeze,
+  '0': calm_breeze,
+  'J': extreme_sun,
+  'Q': spice_clouds,
+  'K': noxious_miasma,
+  'x': rain
+}
