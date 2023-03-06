6ccf54876c47ad38535bb9f6ce9eac6914b20468
Neal Fultz <nfultz@gmail.com>
2023-03-06 00:38:44 +0000

    stubbing out equipment and glider supplies

---
+++ components/equipment.py
@@ -0,0 +1,199 @@
+
+def _equip(f, name, cost):
+    f.name = name
+    f.cost = cost
+    items.append(f)
+    return f
+
+
+@_equip("Snack Containers",25)
+def snack(entity):
+    pass
+
+@_equip("Scavengers' Modular Storage", 25)
+def f(entity):
+    pass
+
+@_equip("Desert Campsite Toolkit", 50)
+def f(entity):
+    pass
+
+@_equip("Weather Monitor", 60)
+def f(entity):
+    pass
+
+@_equip("Explorer Sling", 75)
+def f(entity):
+    pass
+
+@_equip("Heat Regulator", 80)
+def f(entity):
+    pass
+
+@_equip("Medical Pack", 90)
+def f(entity):
+    pass
+
+@_equip("Air Purifier Mask", 95)
+def f(entity):
+    pass
+
+@_equip("Improved Hot Weather Clothing", 100)
+def f(entity):
+    pass
+
+@_equip("Wayfinding Binoculars", 130)
+def f(entity):
+    pass
+
+@_equip("Data Decryption Algo", 140)
+def f(entity):
+    pass
+
+@_equip("Sand Drummer System", 140)
+def f(entity):
+    pass
+
+@_equip("Magnetic Grapple Gloves", 140)
+def f(entity):
+    pass
+
+@_equip("Scrap Detection Unit", 160)
+def f(entity):
+    pass
+
+@_equip("Pathfinder Tools", 175)
+def f(entity):
+    pass
+
+@_equip("Environment Scanner Unit", 180)
+def f(entity):
+    pass
+
+@_equip("Information Visor", 200)
+def f(entity):
+    pass
+
+@_equip("Enhanced Strength Suit Switches", 200)
+def f(entity):
+    pass
+
+@_equip("Technicians' Tookit", 200)
+def f(entity):
+    pass
+
+@_equip("Credit Duplication Program", 250)
+def f(entity):
+    pass
+
+@_equip("Piezo Moisture Capture", 300)
+def f(entity):
+    pass
+
+@_equip("Thermal Cooling Underwear", 360)
+def f(entity):
+    pass
+
+
+### Glider Upgrades
+
+@_equip("Optical Navigation Glasses", 80)
+def f(entity):
+    pass
+
+@_equip("Stabilized Relic Storage", 90)
+def f(entity):
+    pass
+
+@_equip("Anti-Gravity Towing Unit", 120)
+def f(entity):
+    pass
+
+@_equip("Reserve Fuel Canisters", 145)
+def f(entity):
+    pass
+
+@_equip("Fuel Drip Injection Systems", 180)
+def f(entity):
+    pass
+
+@_equip("Long Range Communications", 200)
+def f(entity):
+    pass
+
+@_equip("Scavenger Arm", 260)
+def f(entity):
+    pass
+
+@_equip("Onboard Scanner", 260)
+def f(entity):
+    pass
+
+@_equip("Hacking Module", 260)
+def f(entity):
+    pass
+
+@_equip("Portable Repair Kit", 300)
+def f(entity):
+    pass
+
+@_equip("Fuel Recycle Kit", 400)
+def f(entity):
+    pass
+
+@_equip("Cargo Upgrade X", 90)
+def f(entity):
+    pass
+
+@_equip("Cargo Upgrade Y", 145)
+def f(entity):
+    pass
+
+@_equip("Cargo Upgrade Z", 215)
+def f(entity):
+    pass
+
+@_equip("Fuel Capacity Upgrade X", 110)
+def f(entity):
+    pass
+
+@_equip("Fuel Capacity Upgrade Y", 140)
+def f(entity):
+    pass
+
+@_equip("Fuel Capacity Upgrade Z", 180)
+def f(entity):
+    pass
+
+@_equip("Relic Capacity Upgrade X", 100)
+def f(entity):
+    pass
+
+@_equip("Relic Capacity Upgrade Y", 200)
+def f(entity):
+    pass
+
+@_equip("Relic Capacity Upgrade Z", 300)
+def f(entity):
+    pass
+
+
+@_equip("Top Speed Upgrade X", 120)
+def f(entity):
+    pass
+
+@_equip("Top Speed Upgrade Y", 240)
+def f(entity):
+    pass
+
+@_equip("Top Speed Upgrade Z", 420)
+def f(entity):
+    pass
+
+
+
+
+
+@_equip()
+@_equip()
+@_equip()
